95487e70f9432faa8dc8da4fec72223933cc5797
manu <manu13@gmail.com>
2023-01-30 11:01:46 +0530

    keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "new" {
-  key_name = "new"
+  key_name   = "Firstkey"
   public_key = "file(var.PUB_KEY_PATH)"
 }
--- vars.tf
+++ vars.tf
@@ -1,9 +1,9 @@
-variable AWS_REGION {
+variable "AWS_REGION" {
   default = "eu-west-1"
 }
 
-variable AMIS {
-  type = map
+variable "AMIS" {
+  type = map(any)
   default = {
     eu-west-1  = "ami-0c68b55d1c875067e"
     us-east-1  = "ami-0aa7d40eeae50c9a9"
@@ -11,87 +11,87 @@ variable AMIS {
   }
 }
 
-variable PRIV_KEY_PATH {
+variable "PRIV_KEY_PATH" {
   default = "new"
 }
 
-variable PUB_KEY_PATH {
+variable "PUB_KEY_PATH" {
   default = "new.pub"
 }
 
-variable USERNAME {
+variable "USERNAME" {
   default = "ubuntu"
 }
 
-variable MYIP {
+variable "MYIP" {
   default = "122.171.16.54/32"
 }
 
-variable rmquser {
+variable "rmquser" {
   default = "rabbit"
 }
 
-variable rmqpass {
+variable "rmqpass" {
   default = "Gr33n@pple123456"
 }
 
-variable dbuser {
+variable "dbuser" {
   default = "admin"
 }
 
-variable dbpass {
+variable "dbpass" {
   default = "admin123"
 }
 
-variable dbname {
+variable "dbname" {
   default = "accounts"
 }
 
-variable instance_count {
+variable "instance_count" {
   default = "1"
 }
 
-variable VPC_NAME {
+variable "VPC_NAME" {
   default = "First-VPC"
 }
 
-variable Zone1 {
+variable "Zone1" {
   default = "eu-west-1a"
 }
 
-variable Zone2 {
+variable "Zone2" {
   default = "eu-west-1b"
 }
 
-variable Zone3 {
+variable "Zone3" {
   default = "eu-west-1c"
 }
 
-variable VpcCIDR {
+variable "VpcCIDR" {
   default = "172.21.0.0/16"
 }
 
 
-variable PubSub1CIDR {
+variable "PubSub1CIDR" {
   default = "172.21.1.0/24"
 }
 
-variable PubSub2CIDR {
+variable "PubSub2CIDR" {
   default = "172.21.2.0/24"
 }
 
-variable PubSub3CIDR {
+variable "PubSub3CIDR" {
   default = "172.21.3.0/24"
 }
 
-variable PrivSub1CIDR {
+variable "PrivSub1CIDR" {
   default = "172.21.4.0/24"
 }
 
-variable PrivSub2CIDR {
+variable "PrivSub2CIDR" {
   default = "172.21.5.0/24"
 }
 
-variable PrivSub3CIDR {
+variable "PrivSub3CIDR" {
   default = "172.21.6.0/24"
 }
