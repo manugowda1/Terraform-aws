57a8abfa47d8fc1edfe10d7c5a183c70f89bac72
manu <manu13@gmail.com>
2023-01-30 11:08:30 +0530

    keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "Firstkey" {
-  key_name   = "Firstkey"
-  public_key = "file(var.PUB_KEY_PATH)"
+resource "aws_key_pair" "newkey" {
+  key_name   = "newkey"
+  public_key = file(var.PUB_KEY_PATH)
 }
