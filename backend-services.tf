58d782f666ba91795fe30eae2b90471d28b7b54a
manu <manu13@gmail.com>
2023-01-30 12:35:12 +0530

    edited

--- backend-services.tf
+++ backend-services.tf
@@ -6,7 +6,7 @@ resource "aws_db_subnet_group" "RDS-subgrp" {
   }
 }
 
-resource "aws_elasticache_subnet_group" "Elasticache-subgrp" {
+resource "aws_elasticache_subnet_group" "elasticache-subgrp" {
   name       = "elasticache-subgrp"
   subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
 
@@ -29,15 +29,15 @@ resource "aws_db_instance" "RDS" {
   vpc_security_group_ids = [aws_security_group.Backend-sg.id]
 }
 
-resource "aws_elasticache_cluster" "Elasticache" {
-  cluster_id           = "Elasticache"
+resource "aws_elasticache_cluster" "elasticache" {
+  cluster_id           = "elasticache"
   engine               = "memcached"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.memcached1.5"
   port                 = 11211
   security_group_ids   = [aws_security_group.Backend-sg.id]
-  subnet_group_name    = aws_elasticache_subnet_group.Elasticache-subgrp.name
+  subnet_group_name    = aws_elasticache_subnet_group.elasticache-subgrp.name
 }
 
 resource "aws_mq_broker" "RMQ" {
