d3c8ee55309add480ede44e73f1368b1d6041e67
manu <manu13@gmail.com>
2023-01-30 11:45:10 +0530

    backend-services

---
+++ backend-services.tf
@@ -0,0 +1,55 @@
+resource "aws_db_subnet_group" "RDS-subgrp" {
+  name       = "main"
+  subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+  tags = {
+    Name = "Subnet group for RDS"
+  }
+}
+
+resource "aws_elasticache_subnet_group" "Elasticache-subgrp" {
+  name       = "elasticache-subgrp"
+  subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+
+}
+
+resource "aws_db_instance" "RDS" {
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "5.6.34"
+  instance_class         = "db.t2.micro"
+  name                   = var.dbname
+  username               = var.dbuser
+  password               = var.dbpass
+  parameter_group_name   = "default.mysql5.6"
+  multi_az               = "false"
+  publicly_accessible    = "false"
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.RDS-subgrp.name
+  vpc_security_group_ids = [aws_security_group.Backend-sg.id]
+}
+
+resource "aws_elasticache_cluster" "Elasticache" {
+  cluster_id           = "Elasticache"
+  engine               = "memcached"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.memcached1.5"
+  port                 = 11211
+  security_group_ids   = [aws_security_group.Backend-sg.id]
+  subnet_group_name    = aws_elasticache_subnet_group.Elasticache-subgrp.name
+}
+
+resource "aws_mq_broker" "RMQ" {
+  broker_name        = "RMQ"
+  engine_type        = "ActiveMQ"
+  engine_version     = "5.15.0"
+  host_instance_type = "mq.t2.micro"
+  security_groups    = [aws_security_group.Backend-sg.id]
+  subnet_ids         = [module.vpc.private_subnets[0]]
+
+  user {
+    username = var.rmquser
+    password = var.rmqpass
+  }
+}
